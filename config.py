b95d6632a91b81ef8300edeea98147d0cd124296
ismailhiz <isomaohiz123@gmail.com>
2025-04-21 23:14:27 +0300

    tokenlar kaldırıldı

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # API Key'ler ve sabitler
-TELEGRAM_TOKEN = "7711226117:AAEpa4WQW6LEUl4tbGi1IxnmUJM-kPkoaOA"
-FINNHUB_API_KEY = "d02n73hr01qi6jgi4mr0d02n73hr01qi6jgi4mrg"
+TELEGRAM_TOKEN = ""
+FINNHUB_API_KEY = ""
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
